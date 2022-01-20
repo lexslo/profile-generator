d43f4a6c1e693b787dba723ac563c39d6579e827
Sparkle Party <lexi.slovik@gmail.com>
2022-01-19 22:06:05 -0800

    fix HTML syntax errors in generateHTML function, update CSS

--- src/utils.js
+++ src/utils.js
@@ -33,8 +33,8 @@ function generateHTML(teamObj) {
     // create array of HTML
     const html = [
     `
-    <!DOCTYPE html>
-    <html lang="en">
+  <!DOCTYPE html>
+  <html lang="en">
   
     <head>
       <meta charset="UTF-8">
@@ -42,10 +42,8 @@ function generateHTML(teamObj) {
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>My Team - Portfolio</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" integrity="sha512-UJfAaOlIRtdR+0P6C3KUoTDAxVTuy3lnSXLyLKlHYJlcSU8Juge/mjeaxDNMlw9LgeIotgz5FP8eUQPhX1q10A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="style.css">
-    </head>a
+      <link rel="stylesheet" href="styles.css">
+    </head>
   
     <body>
       <div class="container">
@@ -53,7 +51,7 @@ function generateHTML(teamObj) {
             <h1>My Team</h1>
         </header>
         <main>
-        <div class="row">
+          <div class="row">
       `,
     ];
       
@@ -72,10 +70,10 @@ function generateHTML(teamObj) {
         }
       // add employee cards one by one with their corresponding information
         html.push(`
-          <div class="card medium cyan ligthen-5 z-depth-3 z-depth-3 col s12 m6 l4 xl3">
+          <div class="card medium z-depth-3 col s12 m6 l4 xl3">
             <div class="card-content">
               <span class="card-title"><h2>${employee.name}</h2></span>
-              <span class="card-title subtitle"><h4>${employee.role}<h4></span>
+              <span class="card-title subtitle"><h4>${employee.role}</h4></span>
               <ul>
                 <li>ID: ${employee.id}</li>
                 ${employeeDetail}
@@ -92,12 +90,12 @@ function generateHTML(teamObj) {
       `
         </div>
       </main>
-      <footer class="footer-copyright center-align amber lighten-5">
-        <h5>&copy; ${new Date().getFullYear()} by Lex Slovik</h5>
-      </footer>
+        <footer class="footer-copyright center-align amber lighten-5">
+          <h5>&copy; ${new Date().getFullYear()} by Lex Slovik</h5>
+        </footer>
       </div>
-      </body>
-      </html>
+    </body>
+  </html>
     `);
 
     return html.join('');
